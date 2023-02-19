93c1eee330d8971dceb729be2704bcbd554127dd
pitchakorn thiprangsi <kokejohh@gmail.com>
2023-02-20 01:57:57 +0700

    add all q2 80%

---
+++ q2/circularLinkedList.h
@@ -0,0 +1,11 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+void addAtFront(int client, int length, char *line);
+void addAtLast(int client, int length, char *line);
+void deleteFirst();
+void deleteLast();
+void viewList();
+void test();
+
+#endif
